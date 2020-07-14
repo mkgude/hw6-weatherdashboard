b6bc8383e7d28be41449c3bac7acdf3d57271ba5
Marian Gude <mkgude@gmail.com>
2020-07-14 18:32:31 -0500

    made updates to files

--- weather.js
+++ weather.js
@@ -1,12 +1,114 @@
-// API Key by city
+$(document).ready(() => {
+  console.log("ready");
+  // API Key by city
+  // Dependencies
+  // DOM elements ===============
+  // Starter Data ==============
+  var cities = [];
+  // Functions======================
+  // Display Data ==========
 
-var cityName = "New York";
-var queryURL =
-  "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=c7629276d88b73d9dee17485c554906b"
+  // User Functions ==============
+  console.log($(".submit"));
+  // User types a city
+  //   $(".city");
+  // Submits their search city
+  $(".submit").on("click", function () {
+    var cityNameInput = $("#city-name").val();
+    console.log(cityNameInput);
+    var queryURL =
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
+      cityNameInput +
+      "&units=imperial&appid=c7629276d88b73d9dee17485c554906b";
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (result) {
+      console.log(result);
 
-  $.ajax({
-    url: queryURL
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
+      //  Current weather displays
+      //      city name
+      var cityName = result.city.name;
+      $("#current-city").text(cityName);
+      //      the date
+      var date = moment();
+      var dateDisplay = date.format("MMM Do YYYY");
+      console.log(dateDisplay);
+      $("#current-date").text(dateDisplay);
+      //      an icon representation of weather conditions
+      var iconCode = result.list[0].weather[0].icon;
+      var iconImage = "http://openweathermap.org/img/w/" + iconCode + ".png";
+      console.log(iconCode);
+      console.log(iconImage);
+      $("#current-weather-icon").attr("src", iconImage);
+      //      the temperature
+      var currentTemp = result.list[0].main.temp;
+      console.log(currentTemp);
+      $("#current-temp").text(currentTemp);
+      //      the humidity
+      var currentHumidity = result.list[0].main.humidity;
+      console.log(currentHumidity);
+      $("#current-humidity").text(currentHumidity);
+      //      the wind speed
+      var currentWindSpeed = result.list[0].wind.speed;
+      console.log(currentWindSpeed);
+      $("#current-windspeed").text(currentWindSpeed);
+      //      the UV index
+      var lat = result.city.coord.lat;
+      var lon = result.city.coord.lon;
+      //          The UV Index color coding
+      var uvQueryURL =
+        "http://api.openweathermap.org/data/2.5/uvi?appid=c7629276d88b73d9dee17485c554906b&lat=" +
+        lat +
+        "&lon=" +
+        lon;
+      $.ajax({
+        url: uvQueryURL,
+        method: "GET",
+      }).then(function (result) {
+        console.log(result);
+        var uvIndex = result.value;
+        console.log(typeof uvIndex);
+        var uvIndexValue = Math.floor(parseInt(uvIndex));
+        console.log(uvIndexValue);
+        $("#current-uvindex").text(uvIndex);
+        //      presented with a color that indicates whether the conditions are favorable, moderate, or severe
+        if (uvIndexValue <= 3) {
+          $("#current-uvindex").css("background-color", "green");
+        } else if (uvIndexValue >= 7) {
+          $("#current-uvindex").css("background-color", "red");
+        } else {
+          $("#current-uvindex").css("background-color", "yellow");
+        }
+      });
+      //  Future weather displays
+      //      5-day forecast that displays:
+      //          the date
+      forEach(futureDates());
+      $("#date-one").text(result.list[7].dt_txt);
+      //          an icon representation of weather conditions
+      //          the temperature
+      //          the humidity
+    });
+    // function (citySearchHistory() {
+    //     for (var i = 0; i < cities.length; i++) {
+    //         var cityHistory = cities[i];
+
+    //         var li = document.createElement("li");
+    //         li.textContent = city;
+    //         li.setAttribute("data-index", i);
+
+    //         cityHistory.appendChild(li);
+    //       }
+
+    //     }
+    // });
+
+    //  Search history is saved
+    //  Search history saved below
   });
+  // I click on a city in the search history
+
+  // I open the weather dashboard
+  // I am presented with the last searched city forecast
+});
